d514a31e86d6ea5196c513f1fe32e35f196fd76b
elvxk <pradiptaramadan9c@gmail.com>
2024-10-23 18:46:02 +0700

    💡 [update] get projects

--- src/app/v1/route.js
+++ src/app/v1/route.js
@@ -1,3 +1,10 @@
+import prisma from "@/libs/prisma";
+
 export async function POST(request) {
-  return new Response("Hello, API!");
+  const getProjects = await prisma.projects.findMany();
+  if (!getProjects) {
+    return Response.json({ status: 500, message: "error" });
+  } else {
+    return Response.json({ status: 200, message: "ok", data: getProjects });
+  }
 }
