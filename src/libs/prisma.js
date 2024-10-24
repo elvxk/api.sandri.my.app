29d745a9f8753488c2f8a48ba22329fdd9b75ed6
elvxk <pradiptaramadan9c@gmail.com>
2024-10-24 17:12:00 +0700

    🐛 [fix] prisma

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,12 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL + "?sslmode=require", // SSL mode require
-    },
-  },
-  log: ["query", "info", "warn", "error"],
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
